928bcd772addfa1ddc9f736e17bf0707bc106ef4
Sam <sam@SamdeiMac.local>
2022-04-10 17:01:07 +0800

    meta information

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Sharpermind`,
+    siteTitleAlt: `Sam - Sharpermind & Profession`,
+    siteHeadline: `Sharpermind - Web3 projects from Sam`,
+    siteUrl: `https://sharpermind.asia`,
+    siteDescription: `Keep exploring and innovating to change something`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@samding`,
   },
   plugins: [
     {
